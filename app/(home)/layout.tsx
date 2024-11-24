05d5257d782a5ca15f9bb781be58da798f45d0b1
Ali Kemal Çalak <kemalcalak@gmail.com>
2024-11-24 17:22:21 +0300

    Add Kinde authentication integration and update landing page layout
    - Introduced Kinde authentication by adding dependencies and API route.
    - Updated landing page to include login and registration links.
    - Enhanced layout components to handle user authentication and redirection.
    - Updated .gitignore to include .env files.

--- app/(home)/layout.tsx
+++ app/(home)/layout.tsx
@@ -1,10 +1,19 @@
 import React from "react";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { redirect } from "next/navigation";
 
-const MainLayout = ({
+const MainLayout = async ({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) => {
+  const { isAuthenticated,getUser } = getKindeServerSession();
+  const isUserAuthenticated = await isAuthenticated();
+  const user = await getUser()
+
+  if (!isUserAuthenticated) {
+    redirect("/");
+  }
   return <div>{children}</div>;
 };
 
--- app/(landingPage)/page.tsx
+++ app/(landingPage)/page.tsx
@@ -1,7 +1,13 @@
-import Image from "next/image";
+import {
+  RegisterLink,
+  LoginLink,
+} from "@kinde-oss/kinde-auth-nextjs/components";
 
 export default function Home() {
   return (
-    <div>Landing page</div>
+    <div>
+      <LoginLink>Sign in</LoginLink>
+      <RegisterLink>Sign up</RegisterLink>
+    </div>
   );
 }
