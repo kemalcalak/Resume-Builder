68f4349b0007d0f1f208bd24bf92280fed30cdec
Ali Kemal Çalak <kemalcalak@gmail.com>
2024-11-25 18:13:34 +0300

    Update layout title to "Resume Builder" and modify UUID generation to include prefix

--- lib/helper.ts
+++ lib/helper.ts
@@ -1,6 +1,6 @@
 import { v4 as uuidv4 } from "uuid";
 
 export const generateDocUUID = (): string => {
-  const uuid = uuidv4().replace(/-/g, "");
-  return `${uuid.substring(0, 16)}`;
-};
+    const uuid = uuidv4().replace(/-/g, "");
+    return `doc-${uuid.substring(0, 16)}`;
+  };
