53da302d68dbf5be91695a2bf98a11b0907c1da5
Ali Kemal Çalak <kemalcalak@gmail.com>
2024-11-29 19:46:35 +0300

    Refactor ExperiencePreview component for improved layout; remove unnecessary classes, enhance city and state display, and adjust styling for better readability

--- app/(home)/_components/preview/ExperiencePreview.tsx
+++ app/(home)/_components/preview/ExperiencePreview.tsx
@@ -30,25 +30,28 @@ const ExperiencePreview: FC<PropsType> = ({ resumeInfo, isLoading }) => {
         }}
       />
 
-      <div className="flex flex-col gap-4 mobile:gap-3 min-h-9">
+      <div className="flex flex-col min-h-9">
         {resumeInfo?.experiences?.map((experience, index) => (
           <div 
             key={index} 
-            className="bg-white shadow-sm rounded-lg p-3 mobile:p-2 mobile:rounded-md"
+            className=" rounded-lg  mobile:rounded-md"
           >
+            <div className="flex justify-between">
             <h5 
               className="text-[15px] mobile:text-[14px] font-bold mb-1" 
               style={{ color: themeColor }}
             >
               {experience?.title}
             </h5>
+            <h5 className="text-[13px] mobile:text-[12px] text-gray-700 pt-1 mb-1 mobile:mb-0">
+            {experience?.city}
+                {experience?.city && experience?.state && ", "}
+                {experience?.state}
+            </h5>
+            </div>
             <div className="flex flex-col mobile:flex-row mobile:items-center mobile:justify-between mb-2">
               <h5 className="text-[13px] mobile:text-[12px] text-gray-700 mb-1 mobile:mb-0">
                 {experience?.companyName}
-                {experience?.companyName && experience?.city && ", "}
-                {experience?.city}
-                {experience?.city && experience?.state && ", "}
-                {experience?.state}
               </h5>
               <span className="text-[13px] mobile:text-[12px] text-gray-600">
                 {experience?.startDate}
@@ -57,7 +60,7 @@ const ExperiencePreview: FC<PropsType> = ({ resumeInfo, isLoading }) => {
               </span>
             </div>
             <div
-              className="text-[13px] mobile:text-[12px] leading-tight text-gray-800 exp-preview"
+              className="text-[13px] mobile:text-[12px] leading-tight text-gray-800 exp-preview pb-1"
               dangerouslySetInnerHTML={{
                 __html: experience?.workSummary || "",
               }}
