e1d9cb334bbface0c32d87105719741357c06e71
Ali Kemal Çalak <kemalcalak@gmail.com>
2024-12-01 18:56:35 +0300

    Remove shadow effect from landing page image container for a cleaner design

--- app/(landingPage)/page.tsx
+++ app/(landingPage)/page.tsx
@@ -47,7 +47,7 @@ export default function Home() {
         </div>
         <div className="w-full relative max-w-5xl mx-auto px-4 md:px-6 lg:px-8">
           <div className="absolute top-10 left-1/2 transform -translate-x-1/2 w-full h-[400px] bg-gradient-to-r from-primary to-blue-500 rounded-full blur-3xl opacity-40 z-0" />
-          <div className="w-full h-[400px] md:h-[500px] lg:h-[600px] rounded-xl shadow-lg bg-background">
+          <div className="w-full h-[400px] md:h-[500px] lg:h-[600px] rounded-xl bg-background">
             <div className="relative w-full h-full rounded-md">
               <Image
                 src={imageSrc}
